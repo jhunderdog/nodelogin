19a5af490bb67f63c26d5b7990a95331b92d86c5
jh <33382water@gmail.com>
2019-03-16 13:39:41 +0900

    router

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 
-// const morgan = require('morgan');
-// const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 // const mongoose = require('mongoose');
-// const userRouter = require('./routes/users');
+const userRouter = require('./routes/users');
+
+
+//Middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+//Router
+app.use('/users', userRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
 console.log(`Server Listening at ${PORT}`);
+
+
+
+
